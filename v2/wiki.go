833fa79ae4b0960ec0de253dfb51d59e5779ad27
Elozzy <st.eloghosa@gmail.com>
2020-04-28 18:13:06 +0100

    added a function to handle non-existence pages

--- v2/wiki.go
+++ v2/wiki.go
@@ -2,10 +2,11 @@ package main
 
 
 import (
-	"fmt"
+	
 	"io/ioutil"
 	"net/http"
 	"log"
+	"html/template"
 )
 
 
@@ -35,16 +36,40 @@ func loadPage(title string) (*Page, error) {
 //to view wiki page
 func viewHandler(w http.ResponseWriter, r *http.Request){
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	p, err:= loadPage(title)
+	//if page doesn't exit
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+//edit handler function
+func editHandler(w http.ResponseWriter, r *http.Request){
+	title := r.URL.Path[len("/edit/"):]
+	p, err:= loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	//will read the contents of edit.html 
+	renderTemplate(w, "edit", p)
+}
+
+
+//function to handle rendering of template
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
 }
 
 
 func main() {
-	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	// p1.save()
-	// p2, _ := loadPage("TestPage")
-	// fmt.Println(string(p2.Body))
+	//editPage handler
+	http.HandleFunc	("/edit/", editHandler)
+	//savePage Handler
+	//http.HandleFunc	("/save/", saveHandler)
+	//view page handler
 	http.HandleFunc("/view/", viewHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
