85414e2bb6f22d44a2d38f2ad8046b260f8a8be9
Elozzy <st.eloghosa@gmail.com>
2020-04-29 01:28:18 +0100

    deleted a file

--- v3/wiki.go
+++ v3/wiki.go
@@ -2,97 +2,157 @@ package main
 
 
 import (
-	
-	"io/ioutil"
-	"net/http"
-	"log"
+
 	"html/template"
+
+	"io/ioutil"
+
+	"log"
+
+	"net/http"
+
+	"regexp"
+
 )
 
 
-// to initialize parsed files
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
-
 type Page struct {
+
 	Title string
+
 	Body  []byte
+
 }
 
 
 func (p *Page) save() error {
+
 	filename := p.Title + ".txt"
+
 	return ioutil.WriteFile(filename, p.Body, 0600)
+
 }
 
 
 func loadPage(title string) (*Page, error) {
+
 	filename := title + ".txt"
+
 	body, err := ioutil.ReadFile(filename)
 
 	if err != nil {
+
 		return nil, err
+
 	}
+
 	return &Page{Title: title, Body: body}, nil
 
 }
 
-//to view wiki page
-func viewHandler(w http.ResponseWriter, r *http.Request){
-	title := r.URL.Path[len("/view/"):]
-	p, err:= loadPage(title)
-	//if page doesn't exit
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+
+	p, err := loadPage(title)
+
 	if err != nil {
+
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+
 		return
+
 	}
+
 	renderTemplate(w, "view", p)
+
 }
 
-//edit handler function
-func editHandler(w http.ResponseWriter, r *http.Request){
-	title := r.URL.Path[len("/edit/"):]
-	p, err:= loadPage(title)
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+
+	p, err := loadPage(title)
+
 	if err != nil {
+
 		p = &Page{Title: title}
+
 	}
-	//will read the contents of edit.html 
+
 	renderTemplate(w, "edit", p)
+
 }
 
-//save page handler
-func saveHandler(w http.ResponseWriter, r *http.Request){
-	title := r.URL.Path[len("/save/"):]
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+
 	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte (body)}
-	err :=p.save()
-	//err handling here
+
+	p := &Page{Title: title, Body: []byte(body)}
+
+	err := p.save()
+
 	if err != nil {
+
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+
 		return
+
 	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)	
+
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+
 }
 
 
-//function to handle rendering of template
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+
 	if err != nil {
+
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 
 	}
+
 }
 
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		m := validPath.FindStringSubmatch(r.URL.Path)
+
+		if m == nil {
+
+			http.NotFound(w, r)
+
+			return
+
+		}
+
+		fn(w, r, m[2])
+
+	}
+
+}
 
 
 func main() {
-	//editPage handler
-	http.HandleFunc	("/edit/", editHandler)
-	//savePage Handler
-	http.HandleFunc	("/save/", saveHandler)
-	//view page handler
-	http.HandleFunc("/view/", viewHandler)
+
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
-
-
